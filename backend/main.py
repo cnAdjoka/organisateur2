602bbd52d113344f36ee88bf6ebe7fe29fb4caa4
Chris Adjoka <ragequit078@gmail.com>
2026-02-21 01:17:12 -0500

    frontend has been set up

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from database import create_db_and_tables
-from routes.heroes import router as hero_router
+from backend.database import create_db_and_tables
+from backend.routes.heroes import router as hero_router
 
 app = FastAPI()
 
